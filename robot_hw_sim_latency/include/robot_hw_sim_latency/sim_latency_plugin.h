70419628c4db0c2b5577385b3a10dae7c4742a8c
nano-meter <pjoejoejoe@gmail.com>
2017-12-05 12:20:13 +0100

    Documentation

--- robot_hw_sim_latency/include/robot_hw_sim_latency/sim_latency_plugin.h
+++ robot_hw_sim_latency/include/robot_hw_sim_latency/sim_latency_plugin.h
@@ -14,12 +14,28 @@ public:
   {
   }
 
+  /*
+  *  Called when the plugin is initialized
+  */
   virtual bool initPlugin(const std::string& robot_namespace, ros::NodeHandle model_nh, int n_dof) = 0;
 
+  /*
+  *  Function implementing delay between Gazebo and ros control, called when RobotHWSimLatency::readSim() 
+  * (the function updating the state of hardware interfaces with data from gazebo)
+  *  is called.
+  */
   virtual std::tuple<ros::Time, ros::Duration, std::vector<double>, std::vector<double>, std::vector<double>>
   delayStates(ros::Time time, ros::Duration period, std::vector<double>& position, std::vector<double>& velocity,
               std::vector<double>& effort) = 0;
 
+
+  /*
+  *  Function implementing delay between Gazebo and ros control, called when RobotHWSimLatency::writeSim() 
+  * (the function applying ros control effort, velocity or position commands on Gazebo joints)
+  *  is called.
+  *  Currently the return values of std::vector<double>& position, std::vector<double>& velocity,
+  *  std::vector<double>& effort (the first three std::vector<double>) are not used.
+  */
   virtual std::tuple<ros::Time, ros::Duration, std::vector<double>, std::vector<double>, std::vector<double>,
                      std::vector<double>, std::vector<double>, std::vector<double>>
   delayCommands(ros::Time time, ros::Duration period, std::vector<double>& position, std::vector<double>& velocity,
